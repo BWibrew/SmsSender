5c28a19640ee603e1cba2763f9239360803cab20
Benjamin Wibrew <benjamin.wibrew@gmail.com>
2020-01-30 18:35:15 +0000

    Install rate limit bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
+    Noxlogic\RateLimitBundle\NoxlogicRateLimitBundle::class => ['all' => true],
 ];
